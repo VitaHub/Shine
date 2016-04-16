9a0446a003b3263f2cd4485fef31be0aaec85231
VitaHub <hubenko_vitalii@mail.ua>
2016-04-16 17:08:44 +0300

    Writing unit tests for angular components done

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,25 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( support/phantomjs-shims.self.js )
+
+Rails.application.config.assets.precompile += %w( jquery/dist/jquery.self.js )
+
+Rails.application.config.assets.precompile += %w( jquery_ujs.self.js )
+
+Rails.application.config.assets.precompile += %w( angular/angular.self.js )
+
+Rails.application.config.assets.precompile += %w( angular_test_app.self.js )
+
+Rails.application.config.assets.precompile += %w( customers_app.self.js )
+
+Rails.application.config.assets.precompile += %w( application.self.js )
+
+Rails.application.config.assets.precompile += %w( spec_helper.self.js )
+
+Rails.application.config.assets.precompile += %w( dummy_spec.self.js )
+
+Rails.application.config.assets.precompile += %w( angular-mocks/angular-mocks.self.js )
+
+Rails.application.config.assets.precompile += %w( customers_app/controllers/customer_search_controller_spec.self.js )
